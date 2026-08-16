62a3dbe2122fb7588c7f586d78abdf75df577582
adhritjp-alt <adhritjp@gmail.com>
2026-08-16 15:58:35 +0400

    Add files via upload

---
+++ Getting_started.py
@@ -0,0 +1,16 @@
+#Activity1
+print("Welocome to the wolrd of coding")
+print(7)
+print("Hello, How are you?")
+
+#Activity2
+x = 9
+print("My age is", x)
+year = 2026
+print("The year is", year)
+name = input("What is your name?")
+print(name)
+
+#Activity3
+import keyword
+print(keyword.kwlist)
