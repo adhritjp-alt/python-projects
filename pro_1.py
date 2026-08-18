15d560ea53b0f831e1e5a6ac0c61eeec5e134e54
adhritjp-alt <adhritjp@gmail.com>
2026-08-18 14:04:25 +0400

    Add files via upload

---
+++ pro_1.py
@@ -0,0 +1,11 @@
+print("My daily routine")
+
+print("I wake up at 6:30 AM")
+
+print("I go to school at 7:15 AM")
+
+print("I play with my friends in the evening")
+
+print("I study 1 hour everyday")
+
+print("I study", 1*7,"in a week")
