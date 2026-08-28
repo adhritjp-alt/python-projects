36e7d0bd73d31273683e34a568ca8901e3d26cd4
Code master <adhritjp@gmail.com>
2026-08-28 12:33:35 +0400

    Add files via upload

---
+++ pro3.py
@@ -0,0 +1,18 @@
+import keyword
+
+person_name = input("Enter your name :")
+goal_name = input("Enter your personal goal :")
+target_month = input("Enter the target month :")
+daily_minutes = 15
+
+print("\nName:", person_name)
+print("Goal:", goal_name)
+print("Target month:", target_month)
+print("Daily practice:", daily_minutes, "minutes")
+
+print("\nMy persnal goal plan\n")
+
+print("Goal status:", end=" Started")
+
+print("Progress remainder:", end=" - ")
+print("Practice everyday!")
