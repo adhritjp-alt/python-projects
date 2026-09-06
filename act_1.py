9141ec31a927ff6df2a8769d9f990fa593e11613
Code master <adhritjp@gmail.com>
2026-09-06 15:44:11 +0400

    Add files via upload

---
+++ act_1.py
@@ -0,0 +1,44 @@
+# x = 5
+
+# if (type(x) is int):
+#     print("True")
+# else:
+#     print("False")
+
+# x = 5.5
+
+# if (type(x) is not float):
+#     print("True")
+# else:
+#     print("False")
+
+# x = 20
+# y = 20
+
+# if (x is y):
+#     print("x & y same identity")
+
+# y = 30
+
+# if (x is not y):
+#     print("x & y different identity")
+
+# a = 10
+# b = -10
+
+# print("a >> 1 = ", a >> 1)
+# print("b >> 1 = ", b >> 1)
+
+# a = 5
+# b = -10
+
+# print("a << 1 = ", a << 1)
+# print("b << 1 = ", b << 1)
+
+print("Enter marks obtained from 5 subjects")
+
+MarkOne = int(input())
+MarkTwo = int(input())
+MarkThree = int(input())
+MarkFour = int(input())
+MarkFive = int(input())
