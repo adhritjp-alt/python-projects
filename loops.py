c0749e9999e8f87663a02055cc9c877b96ff2a5d
Code master <adhritjp@gmail.com>
2026-09-25 14:27:52 +0400

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,10 @@
+# string = input("Please enter your string :")
+
+# string2 = ('')
+
+# for i in string:
+#     string2 = i + string2
+
+# print("/nThe orginal string = ", string)
+# print("/nThe reversed string = ", string2)
+
